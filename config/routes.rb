1c9d3cb96d665c2d945442fbc66996d75172b79f
James <james.ryan.mcguire@gmail.com>
2021-07-01 11:58:45 -0700

    Adds routes for users create and index controller methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "/users", controller: "users", action: "create"
+
+  get "/users", controller: "users", action: "index"
+
 end
